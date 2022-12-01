8ad93cb5ebec0be777180206d42abb6e6ff6f072
ADAMSEN BRANAY.J <118241625+Adamsen7777@users.noreply.github.com>
2022-12-01 17:23:52 +0530

    fruits

---
+++ fruits_project/src/fruits_project/Fruits_project.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fruits_project;
+
+/**
+ *
+ * @author ADAMSEN BRANAY
+ */
+public class Fruits_project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        fruits apple=new fruits();
+        fruits orange=new fruits();
+        fruits mango=new fruits();
+        apple.seeds=6;
+        orange.seeds=12;
+        mango.seeds=1;
+        apple.name="red apple";
+        orange.name="palutha orange";
+        mango.name="monkey mango";
+        apple.ujjtu();
+        orange.ujjtu();
+        mango.ujjtu();
+        
+        
+        
+        
+                
+        
+    }
+    
+}
---
+++ fruits_project/src/fruits_project/fruits.java
@@ -0,0 +1,21 @@
+package fruits_project;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ADAMSEN BRANAY
+ */
+public class fruits {
+ 
+        String name;
+        int seeds;
+        void ujjtu()
+        {
+            System.out.println(name+" has "+seeds+" seeds ");
+        }
+                
+}
