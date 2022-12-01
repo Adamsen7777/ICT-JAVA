27e85aabd1afaed6251d58ae7a781ccd6ba88499
ADAMSEN BRANAY.J <118241625+Adamsen7777@users.noreply.github.com>
2022-12-01 18:00:12 +0530

    BTS

---
+++ BTS_Project/src/bts_project/BTS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bts_project;
+
+/**
+ *
+ * @author ADAMSEN BRANAY
+ */
+public class BTS {
+    String name;
+    int age;
+    void music()
+            
+    {
+          System.out.println(name+" his Age is "+ age);
+    }
+           
+            
+            
+}        
+    
+
---
+++ BTS_Project/src/bts_project/BTS_Project.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bts_project;
+
+/**
+ *
+ * @author ADAMSEN BRANAY
+ */
+public class BTS_Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BTS JK =new BTS();
+        BTS V =new BTS();
+        BTS SUGA= new BTS();
+        JK.name="JUNGHOOK";
+        V.name="KIMTEAYUNG";
+        SUGA.name="SUGA";
+        JK.age=25;
+        V.age=26;
+        SUGA.age=28;
+        JK.music();
+        V.music();
+        SUGA.music();
+        
+                
+        
+        
+        
+        
+    }
+    
+}
