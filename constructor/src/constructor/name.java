03e668c593b9218627b7f98f8312e5012921098d
ADAMSEN BRANAY.J <118241625+Adamsen7777@users.noreply.github.com>
2022-12-07 19:46:56 +0530

    JAVA

---
+++ constructor/src/constructor/name.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package constructor;
+
+/**
+ *
+ * @author ADAMSEN BRANAY
+ */
+public class name {
+    
+    String name;
+    int age;
+    
+    void data()
+    {
+       System.out.println(name + " age is " + age);
+    }
+    
+        name()
+        {
+          name= "GAYATHRI";
+           age= 23;      
+        }
+                
+    
+      
+    
+    
+}
