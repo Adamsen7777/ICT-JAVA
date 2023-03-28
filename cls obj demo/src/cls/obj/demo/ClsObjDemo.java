f9d8a4399d3047adcddc7a76a0d0591a4c31d3f2
ADAMSEN BRANAY.J <118241625+Adamsen7777@users.noreply.github.com>
2023-03-28 07:09:37 +0530

    commit

---
+++ cls obj demo/src/cls/obj/demo/ClsObjDemo.java
@@ -0,0 +1,19 @@
+
+package cls.obj.demo;
+
+
+public class ClsObjDemo {
+
+    
+    public static void main(String[] args) {
+        dogs shepard  = new dogs();
+        
+        shepard.name = "DEFFY";
+        shepard.variety ="GERMAN SHEPARD";
+        shepard.legs = 4;
+        
+        shepard.barking();
+        shepard.model();
+    }
+    
+}
---
+++ cls obj demo/src/cls/obj/demo/dogs.java
@@ -0,0 +1,22 @@
+
+package cls.obj.demo;
+
+
+public class dogs {
+    String name ;
+    String variety ;
+    int legs;
+    
+    
+    void barking()
+    {
+        System.out.println(name + " has " + legs + "legs" );
+        
+    }
+    
+    void model()
+    {
+        System.out.println(name + " is " +variety + " variety");
+    }
+    
+}
