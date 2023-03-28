598e9603c8011bb569e82b693f274784da1ebd43
ADAMSEN BRANAY.J <118241625+Adamsen7777@users.noreply.github.com>
2023-03-28 08:21:52 +0530

    c

---
+++ ARRAY 5/src/array/pkg5/ARRAY5.java
@@ -0,0 +1,21 @@
+
+package array.pkg5;
+
+
+public class ARRAY5 {
+
+    
+    public static void main(String[] args) {
+        int ar []= new int[5];
+        ar[0] =9;
+        ar[1] =8;
+        ar[2] =7;
+        ar[3] =6;
+        ar[4] =5;
+        
+        System.out.println(ar[3]);
+    }
+    
+    
+    
+}
