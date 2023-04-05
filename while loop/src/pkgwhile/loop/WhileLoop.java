4f60290b511820db6716d76c48c315c8c5e7298e
ADAMSEN BRANAY.J <118241625+Adamsen7777@users.noreply.github.com>
2023-04-05 09:22:40 +0530

    Update WhileLoop.java

--- while loop/src/pkgwhile/loop/WhileLoop.java
+++ while loop/src/pkgwhile/loop/WhileLoop.java
@@ -1,22 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package pkgwhile.loop;
 
-/**
- *
- * @author ADAMSEN BRANAY
- */
+
 public class WhileLoop {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         int number =1;
-        while (number <=5){
+        while (number <=10){
             System.out.println(number);
             number++;
         }
