6f29628ffcbdbe1006980fd072dae67e82f926a4
ADAMSEN BRANAY.J <118241625+Adamsen7777@users.noreply.github.com>
2023-04-06 17:25:37 +0530

    commit

---
+++ SORRY/src/sorry/SORRY.java
@@ -0,0 +1,15 @@
+
+package sorry;
+
+
+public class SORRY {
+
+   
+    public static void main(String[] args) {
+       int x;
+       for(x=0; x<=1000;x++){
+       System.out.println("SORRY JESUS");
+    }
+    }
+    
+}
