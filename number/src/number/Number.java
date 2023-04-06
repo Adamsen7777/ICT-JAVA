42189f72fd5d4c48b93c2427dae66efa9e9bb7bc
ADAMSEN BRANAY.J <118241625+Adamsen7777@users.noreply.github.com>
2023-04-06 17:35:07 +0530

    commit

---
+++ number/src/number/Number.java
@@ -0,0 +1,16 @@
+
+package number;
+
+
+public class Number {
+
+   
+    public static void main(String[] args) {
+        
+        for(int number=1; number<=100; number++){
+            System.out.println(number);
+        }
+            
+        }
+    
+}
